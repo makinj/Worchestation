bac0a751b62ab8d2303ccd16fff9f9b318e464ac
Joshua Makinen <makinj64@gmail.com>
2024-11-25 22:46:48 -0500

    gets the start of vault working

--- collections/burp/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-bintar="$HOME/configs/burp/bin.tar.gz"
-
-if [ -f "$bintar" ]; then
-  tar -xzf "$bintar" -C /
-fi
--- collections/ethereum/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt install npm
-sudo npm install -g truffle ganache-cli
